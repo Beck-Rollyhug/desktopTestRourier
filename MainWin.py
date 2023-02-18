af6874e3886657f0da8e6e3d027b750342334ca9
loloxat <125580947+loloxat@users.noreply.github.com>
2023-02-18 19:05:01 +0500

    Add files via upload

---
+++ MainWin.py
@@ -0,0 +1,19 @@
+from PyQt5.QtCore import Qt
+from PyQT5.QtCore import
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        pass
+    def initUI(self):
+        pass
+    def connects(self):
+        pass
+    def show(self):
+        pass
+
