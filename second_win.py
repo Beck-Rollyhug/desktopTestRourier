b34e804612436aca0714d5aba85811c66db83081
FlashkaGame <125580978+FlashkaGame@users.noreply.github.com>
2023-02-18 19:01:26 +0500

    Add files via upload

---
+++ second_win.py
@@ -0,0 +1,20 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+class TestWin(QWidget):
+    def __init__(self):
+        super.__init__()
+        self.setappear()
+        self.initUI()
+        self.connects()
+        self.show()
+        
+    def setappear(self):
+        pass
+    def initUI(self):
+        pass
+    def connects(self):
+        pass
+    def show(self):
+        pass
+    def Function5(self):
+        pass
