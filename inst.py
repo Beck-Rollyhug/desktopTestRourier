ccab6b46f3d11095b1661cb2a4fe7225978749d8
ArtemRum <125743824+ArtemRum@users.noreply.github.com>
2023-02-24 15:46:41 +0500

    Update inst.py

--- inst.py
+++ inst.py
@@ -1,7 +1,10 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import *
 
 txt_title = 'Непонятная вещь с неизмеримо огромным недовольством'
+txt_hello = 'Добро пожалевать в программу по определению состояния здоровья.'
+txt_instuction = ''
+txt_fio = ''
 
 win_x, win_y = 200, 100
 win_wight, win_height = 1000, 600
