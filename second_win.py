2ab4e1324bc47a4a23a04976b6756e04a7f5f226
ArtemRum <125743824+ArtemRum@users.noreply.github.com>
2023-02-24 16:02:53 +0500

    Какую-то херню сделал. Витя(сорян, если опять перепутал) разбирайся сам.

--- second_win.py
+++ second_win.py
@@ -1,5 +1,3 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from inst import *
 
 app = QApplication([])
@@ -19,10 +17,50 @@ class TestWin(QWidget):
         self.move(win_x, win_y)
     
     def initUI(self):
-        pass
+        self.h_line = QHBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.l_line = QVBoxLayout()
+        txt1 = QLabel(txt_fio)
+        txt2 = QLabel('')
+        txt3 = QLabel('txt_')
+        txt4 = QLabel('txt_')
+        txt5 = QLabel('txt_')
+        txt6 = QLabel('(txt_')
+        self.button1 = QPushButton('txt_b_')
+        self.button2 = QPushButton('(txt_b_')
+        self.button3 = QPushButton('txt_b_')
+        self.btn_next = QPushButton('txt_b_')
+        pole1 = QLineEdit('txt_l_')
+        pole2 = QLineEdit('txt_l_')
+        pole3 = QLineEdit('txt_l_')
+        pole4 = QLineEdit('txt_l_)')
+        pole5 = QLineEdit('txt_l_')
+        
+        self.r_line.addWidget(txt6)
+        
+        self.l_line.addWidget(txt1)
+        self.l_line.addWidget(txt2)
+        self.l_line.addWidget(txt3)
+        self.l_line.addWidget(txt4)
+        self.l_line.addWidget(txt5)
+        self.l_line.addWidget(self.button1)
+        self.l_line.addWidget(self.button2)
+        self.l_line.addWidget(self.button3)
+        self.l_line.addWidget(pole1)
+        self.l_line.addWidget(pole2)
+        self.l_line.addWidget(pole3)
+        self.l_line.addWidget(pole4)
+        self.l_line.addWidget(pole5)
+        
+        self.h_line.addLayout(r_line)
+        self.h_line.addLayout(l_line)
     
     def connects(self):
-        pass
+        self.btn_next.clicked.connect(self.next_click)
+        
+    def next_click(self):
+        self.hide()
+        self.fw = FinalWin()
     
     def show(self):
         pass
