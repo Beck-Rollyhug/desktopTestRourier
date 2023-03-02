93e8e20f11151d16af919ed00661b6cf9dcd2029
Misha <mihail.laz5213@gmail.com>
2023-03-02 20:36:26 +0500

    made ManeWIn

--- MainWin.py
+++ MainWin.py
@@ -1,12 +1,15 @@
 from PyQt5.QtCore import Qt
-from PyQT5.QtCore import 
-from inst import*
+from PyQt5.QtWidgets import *
+from inst import *
 
 #TODO: Написать функцию initUI
 #TODO: Написать функцию connects
 #TODO: Написать функцию show
 #TODO: Проверить файл MainWIn на работоспособность(Первый экран должен открываться при запуске)
 
+txt_title = "Здоровье"
+win_width, win_height = 1000, 600
+win_x, win_y = 200, 200
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -14,15 +17,28 @@ class MainWin(QWidget):
         self.initUI()
         self.connects()
         self.show()
+    def next_win(self):
+        self.hide()
+        self.tw = TestWin()
     def set_appear(self):
         self.setWindowTtle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
 
     def initUI(self):
-        pass
+        self.hello_txt = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button_next = QPushButton(txt_next)
+        self.v_line = QVBoxLayout()
+        self.v_line.addWidget(self.hello_txt, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.v_line.addWidget(self.button_next, alignment = Qt.AlignCenter)
     def connects(self):
-        pass
+        self.button_next.clicked.connect(self.next_win)
     def show(self):
-        pass
+        self.show()
 
+#def main():
+app = QApplication([])
+main_win = MainWin()
+app.exec_()
