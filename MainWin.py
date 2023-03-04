d396bf8174902f4389a10e13ea53bfa6144770d3
Aleksey <my.beck.for.work@mail.ru>
2023-03-04 12:18:56 +0500

    fix

--- MainWin.py
+++ MainWin.py
@@ -1,13 +1,16 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (
-       QApplication, QWidget,
-       QHBoxLayout, QVBoxLayout,
-       QGroupBox, QRadioButton,
-       QPushButton, QLabel, QListWidget, QLineEdit)
-
+from PyQt5.QtWidgets import *
 from inst import *
 from second_win import *
 
+#TODO: Написать функцию initUI
+#TODO: Написать функцию connects
+#TODO: Написать функцию show
+#TODO: Проверить файл MainWIn на работоспособность(Первый экран должен открываться при запуске)
+
+txt_title = "Здоровье"
+win_width, win_height = 1000, 600
+win_x, win_y = 200, 200
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -34,14 +37,18 @@ class MainWin(QWidget):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
-
     def initUI(self):
-        pass
+        self.hello_txt = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button_next = QPushButton(txt_next)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_txt, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.button_next, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout)
     def connects(self):
-        pass
-    def show(self):
-        pass
+        self.button_next.clicked.connect(self.next_win)
 
 app = QApplication([])
-mw = MainWin()
+main_win = MainWin()
 app.exec_()
