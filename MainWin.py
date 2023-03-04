345e8f037b15897d1285238fb9497a4fe1a790a8
Aleksey <my.beck.for.work@mail.ru>
2023-03-04 11:45:47 +0500

    fix update

--- MainWin.py
+++ MainWin.py
@@ -1,16 +1,37 @@
 from PyQt5.QtCore import Qt
-from PyQT5.QtCore import 
-from inst import*
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+
+from inst import *
+from second_win import *
 
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
+       
+        # создаём и настраиваем графические элементы:
         self.set_appear()
+
+        # устанавливает связи между элементами
         self.initUI()
+
+        # устанавливает, как будет выглядеть окно (надпись, размер, место)
         self.connects()
+
+        # старт:
         self.show()
+
     def set_appear(self):
-        self.setWindowTtle(txt_title)
+        txt_title = '111'
+        win_x = 0
+        win_y = 0
+        win_width = 600
+        win_height = 400
+
+        self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
 
@@ -21,3 +42,6 @@ class MainWin(QWidget):
     def show(self):
         pass
 
+app = QApplication([])
+mw = MainWin()
+app.exec_()
